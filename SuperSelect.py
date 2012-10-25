09b3f76cef1f8147a7540979cce7a13bf886c3d6
Brad Carson <brad@pixelwavedesign.com>
2012-10-25 13:55:53 -0400

    move defaults into initializer

--- SuperSelect.py
+++ SuperSelect.py
@@ -1,33 +1,37 @@
 import sublime, sublime_plugin
 
 key = "super_select"
-current_selected_region = None
+first_selected_region = None
 last_selected_region = None
 
 class SuperSelect(sublime_plugin.TextCommand):
 
   def run(self, edit):
-    global current_selected_region
+    global first_selected_region, last_selected_region
     # do things like grab selected words, figure start and end, bounds, skipped, wrap, etc.
 
-    print current_selected_region
     if self.view.sel()[0].size() > 0: # don't run if there's no selection
+
+      # set up defaults
+      if first_selected_region == None:
+        first_selected_region = self.view.sel()[0]
+        last_selected_region = self.view.sel()[-1]
+
       self.go(edit)
     else:
-      current_selected_region = None
+      first_selected_region = None
+      last_selected_region = None
 
 
 class SelectPreviousCommand(SuperSelect):
   def go(self, edit):
-    global current_selected_region
+    global first_selected_region
 
     # TODO: we are emulating the way Sublime selects words, wish there was a way to grab Sublime's regions.
     self.view.erase_regions(key)
     # separatorString = self.view.settings().get('word_separators') + u" \n\r"
 
-    first_sel_region = self.view.sel()[0]
-
-    pattern = "(?<!\w)" + self.view.substr(first_sel_region) + "(?![\w])"
+    pattern = "(?<!\w)" + self.view.substr(first_selected_region) + "(?![\w])"
     matching_regions = self.view.find_all(pattern)
 
     print "Matching Regions: ", matching_regions
@@ -36,8 +40,8 @@ class SelectPreviousCommand(SuperSelect):
     self.view.add_regions(key, matching_regions, 'comment', sublime.DRAW_OUTLINED)
 
     # back up one to previous match
-    prev_region_index = matching_regions.index(current_selected_region or first_sel_region) - 1
-    current_selected_region = matching_regions[prev_region_index]
+    prev_region_index = matching_regions.index(first_selected_region) - 1
+    first_selected_region = matching_regions[prev_region_index]
     print prev_region_index
     print matching_regions[prev_region_index]
 
@@ -48,16 +52,12 @@ class SelectPreviousCommand(SuperSelect):
 # Selects next word relative to last selection
 class ExpandNextCommand(SuperSelect):
   def go(self, edit):
-    global current_selected_region, last_selected_region
+    global first_selected_region, last_selected_region
 
     # find all regions matching currently selected text
     selected_text = self.view.substr(self.view.sel()[0])
     pattern = "(?<!\w)" + selected_text + "(?![\w])"
     matching_regions = self.view.find_all(pattern)
-
-    # default to last selection, otherwise we store it later for wrap-around cases
-    if last_selected_region == None:
-      last_selected_region = self.view.sel()[-1]
 
     # find next occurence AFTER last_selected_region in matching_regions
     region_count = len(matching_regions)
