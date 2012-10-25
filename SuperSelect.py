2bb4816ee3f717069aaf2d79a5d471c028d2a127
Brad Carson <brad@pixelwavedesign.com>
2012-10-25 13:17:46 -0400

    wraparound prev select

--- SuperSelect.py
+++ SuperSelect.py
@@ -1,31 +1,43 @@
 import sublime, sublime_plugin
 
+key = "super_select"
+last_selected_region = None
 
-class SelectPreviousCommand(sublime_plugin.TextCommand):
+class SuperSelect(sublime_plugin.TextCommand):
+
   def run(self, edit):
-    # self.doSearch(self.view, False)
-    # (sublimer).sublimer.
-    # TODO: we are selecting more here than sublime does naturally.. fix this
-    self.view.erase_regions("test")
+    global last_selected_region
+    # do things like grab selected words, figure start and end, bounds, skipped, wrap, etc.
+
+    print last_selected_region
+    if self.view.sel()[0].size() > 0: # don't run if there's no selection
+      self.go(edit)
+    else:
+      last_selected_region = None
+
+
+class SelectPreviousCommand(SuperSelect):
+  def go(self, edit):
+    global last_selected_region
+    # TODO: we are emulating the way Sublime selects words, wish there was a way to grab Sublime's regions.
+    self.view.erase_regions(key)
     # separatorString = self.view.settings().get('word_separators') + u" \n\r"
 
     first_sel_region = self.view.sel()[0]
+
     pattern = "(?<!\w)" + self.view.substr(first_sel_region) + "(?![\w])"
-    print pattern
     matching_regions = self.view.find_all(pattern)
 
     print "Matching Regions: ", matching_regions
 
     # highlight all matching regions # sublime does this by default - (for debugging)
-    self.view.add_regions("test", matching_regions, 'comment', sublime.DRAW_OUTLINED)
+    self.view.add_regions(key, matching_regions, 'comment', sublime.DRAW_OUTLINED)
 
     # back up one to previous match
-    prev_region_index = matching_regions.index(first_sel_region) - 1
+    prev_region_index = matching_regions.index(last_selected_region or first_sel_region) - 1
+    last_selected_region = matching_regions[prev_region_index]
     print prev_region_index
     print matching_regions[prev_region_index]
-    # print [y[0] for y in matching_regions].index(first_sel_region)
 
     # add previous region to selection
     self.view.sel().add(matching_regions[prev_region_index])
-
-    #TODO: fix loop around
